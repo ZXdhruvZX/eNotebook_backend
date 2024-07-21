c3fa1a02d8c20c0c79fc93de57849510f774906e
ZXdhruvZX <dhruvsharma7016@gmail.com>
2024-07-22 02:00:48 +0530

    new commits

---
+++ database/db.js
@@ -0,0 +1,12 @@
+import { connect } from "mongoose";
+import 'dotenv/config';
+const connectToMongo = async () => {
+  try {
+    await connect('process.env.MONGO_URI');
+    console.log("---***Database Connected Successfully***---")
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export default connectToMongo;
